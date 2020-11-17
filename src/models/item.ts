cd56c06167ae7907fef08647544c52b65140325f
Sangbong Lee <sblee@ridi.com>
2020-11-17 19:08:07 +0900

    Update Item object
    - item_repr -> item_episode
    - quantity as required

--- src/models/item.ts
+++ src/models/item.ts
@@ -4,10 +4,10 @@ export interface Item {
   readonly service_type: ServiceType;
   readonly item_id: string;
   readonly item_name: string;
-  readonly item_repr_id?: string;
-  readonly item_repr_name?: string;
+  readonly item_episode_name?: string;
+  readonly item_episode_id?: string;
   readonly item_category: number;
   readonly item_genre?: string;
   readonly price?: number;
-  readonly quantity?: number;
+  readonly quantity: number;
 }
